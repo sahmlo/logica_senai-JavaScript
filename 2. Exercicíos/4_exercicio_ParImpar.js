18b484f27f67b59c5ea5e3f5cf14f0c06a64d541
Aluno Dendezeiros <aluno.den@senaibahia.com.br>
2025-06-11 15:46:57 -0300

    Exercícios de JavaScript

--- 2. Exercicíos/4_exercicio_ParImpar.js
+++ 2. Exercicíos/4_exercicio_ParImpar.js
@@ -2,16 +2,15 @@
 // npm install readline-sync (Para aparecer no terminal.)
 
 const readlineSync = require('readline-sync');
-soma = 0;
-multiplicacao = 0;
+par = 0
+impar = 0
 
-let numero1 = readlineSync.questionInt('Digite um numero: ');
-let numero2 = readlineSync.questionInt('Digite um numero: ');
+let numero = readlineSync.questionInt('Digite um numero: ');
 
-if (numero1 == numero2) {
-    soma = numero1 + numero2
-    console.log(`A soma dos números é ${soma}`)
-} else if (numero1 != numero2) {
-    multiplicacao = numero1 * numero2
-    console.log(`A multiplicação dos números é ${multiplicacao}`)    
+if (numero == 0) {
+    console.log(`Esse numero é neutro.`)    
+} else if (numero % 2 == 0) {
+    console.log(`Seu numero é par.`)
+} else {
+    console.log(`Seu numero é impar.`)
 }
